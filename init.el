a68c979d91879a09d6797205ebc6dec9ae185e65
Saeed Afzal <saeed.afzal@protonmail.com>
2025-02-03 11:08:53 +0000

    Custom file, and use-package for packages

--- init.el
+++ init.el
@@ -1,8 +1,13 @@
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
 (setq inhibit-startup-message t) ; Disable splash screen
 (setq display-line-numbers-type 'relative) ; Relative line numbers
 (setq indent-tabs-mode nil)
 (setq make-backup-files nil) ; Disable ~ files
 (setq auto-save-default nil) ; Disabel #autosave#
+(setq scroll-step 1
+	  scroll-conservatively 10000)
 
 (setq-default tab-width 4)
 
@@ -10,34 +15,38 @@
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
 (ido-mode)
+(global-display-line-numbers-mode)
+(load-theme 'modus-vivendi t)
 
 (add-to-list 'default-frame-alist
 			 '(font . "JetBrainsMono Nerd Font-14")
 			 '(font . "JetBrainsMono NF-14"))
-
-(global-display-line-numbers-mode) ; Line numbers
-
-(load-theme 'modus-vivendi t)
 
 ;; Melpa
 (require 'package)
 (add-to-list 'package-archives
 			 '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-;; (package-refresh-contents)
+
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 ;; Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-(require 'evil)
-(evil-mode 1)
-(evil-set-initial-state 'dired-mode 'emacs) ; Use emacs state instead of evil in dired
+(setq evil-want-keybinding nil)
+
+(use-package evil
+  :config
+  (evil-mode))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
 ;; Magit
-(unless (package-installed-p 'magit)
-  (package-install 'magit))
+(use-package magit)
 
 ;; Go mode
-(unless (package-installed-p 'go-mode)
-  (package-install 'go-mode))
-(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
+(use-package go-mode
+  :config
+  (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode)))
