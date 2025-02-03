0495986ea8501e1cb2d5e612fa913cdfa4f879cf
Saeed Afzal <saeed.afzal@protonmail.com>
2025-02-03 09:22:15 +0000

    Fonts, tabs, go mode

--- init.el
+++ init.el
@@ -1,14 +1,19 @@
 (setq inhibit-startup-message t) ; Disable splash screen
 (setq display-line-numbers-type 'relative) ; Relative line numbers
 (setq indent-tabs-mode nil)
-(setq tab-width 4)
 (setq make-backup-files nil) ; Disable ~ files
 (setq auto-save-default nil) ; Disabel #autosave#
+
+(setq-default tab-width 4)
 
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 (scroll-bar-mode 0)
-(ido-mode 1)
+(ido-mode)
+
+(add-to-list 'default-frame-alist
+			 '(font . "JetBrainsMono Nerd Font-14")
+			 '(font . "JetBrainsMono NF-14"))
 
 (global-display-line-numbers-mode) ; Line numbers
 
@@ -17,7 +22,7 @@
 ;; Melpa
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/"))
+			 '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 ;; (package-refresh-contents)
 
@@ -31,3 +36,8 @@
 ;; Magit
 (unless (package-installed-p 'magit)
   (package-install 'magit))
+
+;; Go mode
+(unless (package-installed-p 'go-mode)
+  (package-install 'go-mode))
+(add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
